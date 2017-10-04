3b223661d4d3967f3b83e6af24a027c1ab8d6b1d
tycoon87 <tyson.woodruff@outlook.com>
2017-10-03 17:15:02 -0700

    instrutors not working

---
+++ names.py
@@ -0,0 +1,37 @@
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+#def names(x):
+#    for i in range(0,len(x)):
+#        print x[i]['first_name'], x[i]['last_name']
+#names(students)
+                                            
+def name(x):
+    for i in x: 
+        print i
+        if i == 'Students':
+            print "i is == students"
+            for j in x[i]:
+#                print j
+                print j['first_name'], j['last_name']
+        elif i == ['Instructors']:
+            print "i is == instructors"
+            for j in x[i]:
+                print j
+                print j['first_name'], [j]['last_name']
+name(users)
